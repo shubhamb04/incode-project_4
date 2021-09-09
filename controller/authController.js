5e747804fb40b7a6fc67b8e949eb34ec714f2092
Shubham Bhavsar <shubhambhavsar50@gmail.com>
2021-09-09 23:17:31 +1000

    created signup post route

--- controller/authController.js
+++ controller/authController.js
@@ -1,3 +1,5 @@
+const db = require("../db/database")
+
 module.exports.signup_get = (req, res) => {
     res.render('../view/pages/signup')
 }
@@ -6,9 +8,27 @@ module.exports.login_get = (req, res) => {
     res.render('../view/pages/login')
 }
 
-module.exports.signup_post = (req, res) => {
-    const { email, password } = req.body;
-    res.send('new sign up')
+module.exports.signup_post = async (req, res) => {
+    try {
+
+        //destructure the inputs
+        const { firstname, lastname, email, password, confirm_password } = req.body;
+
+        //check if user already exists in db
+        const user = await db.any("SELECT * FROM users WHERE email = $1", [email]);
+
+        if (user.length !== 0) {
+            return res.status(401).send("User already exist!")
+        }
+        
+        //bcrypt the password
+        const hash = await bcrypt.hash(password, 10)
+
+        res.json(user)
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).sned("Server Error")
+    }
 }
 
 module.exports.login_post = (req, res) => {
